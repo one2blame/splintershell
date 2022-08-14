72e283a381d6c5aa05f466083bb8f5cffa6f8f3f
one2blame <54484322+one2blame@users.noreply.github.com>
2022-08-14 16:31:33 -0400

    Improve HTTP packet parsing

--- splinter_shell/protocols/http.py
+++ splinter_shell/protocols/http.py
@@ -1,4 +1,5 @@
-from dpkt import ethernet, pcap
+from scapy.all import *
+from scapy.layers.http import *
 
 from .pcapparser import PcapParser
 
@@ -6,14 +7,19 @@ from .pcapparser import PcapParser
 class HttpPcapParser(PcapParser):
     def parse_pcap(self) -> None:
         try:
-            with self.filepath.open(mode="rb") as pcap_file:
-                pcap_data = pcap.Reader(pcap_file)
-                for _, buf in pcap_data:
-                    protocol_data = ethernet.Ethernet(buf).data.data
-                    if protocol_data.sport == 80 or protocol_data.dport == 80:
-                        http_data = str("".join(map(chr, protocol_data.data)))
-                        if http_data and all(ord(c) < 256 for c in http_data):
-                            self.payload_list.append(http_data)
+            packets = sniff(offline=str(self.filepath))
+
+            for packet in packets:
+                if packet.haslayer(HTTPRequest):
+                    http_data = str("".join(map(chr, bytes(packet[HTTPRequest]))))
+                    if http_data and all(ord(c) < 256 for c in http_data):
+                        self.payload_list.append(http_data)
+
+                if packet.haslayer(HTTPResponse):
+                    http_data = str("".join(map(chr, bytes(packet[HTTPResponse]))))
+                    if http_data and all(ord(c) < 256 for c in http_data):
+                        self.payload_list.append(http_data)
+
         except Exception as e:
             print(f"Failed to read provided file: {str(self.filepath)}")
             raise e
